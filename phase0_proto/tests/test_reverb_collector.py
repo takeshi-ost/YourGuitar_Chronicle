6148a5f436a73d2e74afc237dd38de4043cd6419
takeshi-ost <takeshi.ost@gmail.com>
2026-09-24 11:12:53 +0900

    Test Reverb image endpoint parsing

---
+++ phase0_proto/tests/test_reverb_collector.py
@@ -0,0 +1,52 @@
+from ygc.collectors.reverb import ReverbAPICollector
+
+
+def test_first_image_url_from_images_payload():
+    payload = {
+        "images": [
+            {
+                "id": 87485553,
+                "url": (
+                    "https://images.reverb.com/"
+                    "image/upload/example.png"
+                ),
+            }
+        ]
+    }
+
+    assert (
+        ReverbAPICollector
+        ._first_image_url(
+            payload
+        )
+        == (
+            "https://images.reverb.com/"
+            "image/upload/example.png"
+        )
+    )
+
+
+def test_first_image_url_from_embedded_payload():
+    payload = {
+        "_embedded": {
+            "images": [
+                {
+                    "url": (
+                        "https://images.reverb.com/"
+                        "image/upload/example2.png"
+                    )
+                }
+            ]
+        }
+    }
+
+    assert (
+        ReverbAPICollector
+        ._first_image_url(
+            payload
+        )
+        == (
+            "https://images.reverb.com/"
+            "image/upload/example2.png"
+        )
+    )
